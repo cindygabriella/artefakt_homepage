3f1c8b3d57221f58e81d804a97dfa9339b131207
chrischne <chrischne@gmail.com>
2019-07-23 17:19:42 +0200

    format

--- js/fall2_test.js
+++ js/fall2_test.js
@@ -1,114 +1,114 @@
 var sketch2_test = function (s) {
 
-var data = [];
-var ready = false;
+  var data = [];
+  var ready = false;
 
-var button;
+  var button;
 
-s.setup = function() {
-  d3.csv("csv/recycling.csv", function (d) {
-    return {
-      Year: +d.Year,
-      Entity: d.Entity,
-      plastic: +d.plastic,
-    };
-  }).then(function (csv) {
-    data = csv;
-    ready = true;
-    s.redraw();
-  });
+  s.setup = function () {
+    d3.csv("csv/recycling.csv", function (d) {
+      return {
+        Year: +d.Year,
+        Entity: d.Entity,
+        plastic: +d.plastic,
+      };
+    }).then(function (csv) {
+      data = csv;
+      ready = true;
+      s.redraw();
+    });
 
-  s.createCanvas(1400, 800);
-  s.textSize(11);
-  //pixelDensity(8);
+    s.createCanvas(1400, 800);
+    s.textSize(11);
+    //pixelDensity(8);
 
-  // test button - Achsen tauschen
-  button = s.createButton('Achsen tauschen');
-  button.position(100, 2200);
-  button.mousePressed(achsentauschen);
-}
-
-  s.draw = function() {
-  if (!ready) {
-    s.background(255, 0, 0);
-    s.noStroke();
-    return;
-  } else {
-    s.background(255);
+    // test button - Achsen tauschen
+    button = s.createButton('Achsen tauschen');
+    button.position(100, 2200);
+    button.mousePressed(achsentauschen);
   }
 
+  s.draw = function () {
+    if (!ready) {
+      s.background(255, 0, 0);
+      s.noStroke();
+      return;
+    } else {
+      s.background(255);
+    }
 
 
-  s.fill('#9dc79d');
-  // stroke(0,0,0);
-  // strokeWeight(0.2);
+
+    s.fill('#9dc79d');
+    // stroke(0,0,0);
+    // strokeWeight(0.2);
 
 
-  //Mismanagedplasticwaste als y-Achse, Entity als X-Achse.
-  // Minimum Mismanagedplasticwaste finden
-  var plasticMin = d3.min(data, function (d) {
-    return d.plastic;
-  });
+    //Mismanagedplasticwaste als y-Achse, Entity als X-Achse.
+    // Minimum Mismanagedplasticwaste finden
+    var plasticMin = d3.min(data, function (d) {
+      return d.plastic;
+    });
 
     //Maximum Mismanagedplasticwaste finden
-  var plasticMax = d3.max(data, function (d) {
-    return d.plastic;
-  })
+    var plasticMax = d3.max(data, function (d) {
+      return d.plastic;
+    })
 
-  var plasticCount = plasticMax - plasticMin;
+    var plasticCount = plasticMax - plasticMin;
 
 
-  var yearMin = d3.min(data, function (d) {
-     return d.Year;
-  });
+    var yearMin = d3.min(data, function (d) {
+      return d.Year;
+    });
 
-  var yearMax = d3.max(data, function (d) {
-     return d.Year;
-  });
+    var yearMax = d3.max(data, function (d) {
+      return d.Year;
+    });
 
-  var yearCount = yearMax - yearMin;
+    var yearCount = yearMax - yearMin;
 
     // "falsch"
     for (var i = 0; i < data.length; i++) {
-    var d = data[i];
+      var d = data[i];
 
 
-   //Jahre auf die y-Achse Mappen
-    var y = s.map(d.Year, yearMin, yearMax, 0, s.height-700);
+      //Jahre auf die y-Achse Mappen
+      var y = s.map(d.Year, yearMin, yearMax, 0, s.height - 700);
 
-    var x = s.map(d.plastic, plasticMin, plasticMax, 0, 750);
+      var x = s.map(d.plastic, plasticMin, plasticMax, 0, 750);
 
-   // var x = s.map(d.Year, yearMin, yearMax, 0, s.width - w - 60);
+      // var x = s.map(d.Year, yearMin, yearMax, 0, s.width - w - 60);
 
-     //Breite des Balkens
-    var h = 15;
+      //Breite des Balkens
+      var h = 15;
 
-    s.push();                    // <- push a drawing context
-    // translate(x, y);        // <- move to position
+      s.push();                    // <- push a drawing context
+      // translate(x, y);        // <- move to position
 
-    s.rect(0-y, s.height-x, x, h);
+      s.rect(0 - y, s.height - x, x, h);
 
-    //s.text(d.plastic, y, x-s.height-10);
-    s.text(d.Year, s.height-y, x);
-    //s.text(d.Year, x,s.height-y-30);
-    s.noStroke();
-    s.pop();                     // <- reset the drawing context
+      //s.text(d.plastic, y, x-s.height-10);
+      s.text(d.Year, s.height - y, x);
+      //s.text(d.Year, x,s.height-y-30);
+      s.noStroke();
+      s.pop();                     // <- reset the drawing context
+    }
+
+    // Make a call to the custom function achsentauschen()
+    achsentauschen()
+
+  };
+
+  function achsentauschen() {
+
   }
 
-  // Make a call to the custom function achsentauschen()
-  achsentauschen()
 
-};
-
-function achsentauschen() {
-
-  }
-
-
-// function trigger() {
-//   console.log('uxRect just got clicked!');
-// }
+  // function trigger() {
+  //   console.log('uxRect just got clicked!');
+  // }
 
 }
 
-new p5(sketch2_test,'grafik2_test');
+new p5(sketch2_test, 'grafik2_test');
